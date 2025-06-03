1fe3f0c1cd247c37be79b054f4ca3e160672aa18
CToID <funk443@yahoo.com.tw>
2025-06-03 17:03:36 +0800

    Add license notice.

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
+# Copyright © 2025 CToID <funk443@yahoo.com.tw>
+#
+# This program is free software. It comes without any warranty, to the
+# extent permitted by applicable law. You can redistribute it and/or
+# modify it under the terms of the Do What The Fuck You Want To Public
+# License, Version 2, as published by Sam Hocevar. See the COPYING file
+# for more details.
+
 import sys
 
 from ai_edge_litert.interpreter import Interpreter
