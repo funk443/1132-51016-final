87bc05e7bfa64de3ee4a20c9dd8ec788da353072
CToID <funk443@yahoo.com.tw>
2025-06-07 03:38:55 +0800

    Revert "Implement output to a network stream."
    This reverts commit 4f6f45cad2471764cd35cc49d5e9c0513715f085.

--- main.py
+++ main.py
@@ -40,12 +40,6 @@ argparser.add_argument(
     type=str,
     metavar="PATH",
 )
-argparser.add_argument(
-    "--output-stream",
-    action="store",
-    type=str,
-    metavar="URL",
-)
 
 INPUT_SIZE = 192
 
@@ -220,13 +214,6 @@ if __name__ == "__main__":
             cap.release()
             exit(1)
 
-        if argv.output_stream is not None:
-            out = cv.VideoWriter(
-                argv.output_stream,
-                cv.VideoWriter_fourcc(*"MP4V"),
-                15,
-                (192, 192),
-            )
         try:
             warnings_count = 0
             while True:
@@ -256,16 +243,10 @@ if __name__ == "__main__":
                 draw_skeleton(
                     frame, actual_positions, thickness * 2, thickness, color
                 )
-                if argv.output_stream is not None:
-                    out.write(frame)
-                    continue
-
                 cv.imshow("HHH", frame)
                 if cv.waitKey(1) == ord("q"):
                     break
 
         finally:
             cap.release()
-            if argv.output_stream is not None:
-                out.release()
             cv.destroyAllWindows()
