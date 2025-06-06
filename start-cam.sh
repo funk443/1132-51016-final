9d798dc0e5dcfb5cbac3ea71477c41df4a00e3d4
CToID <funk443@yahoo.com.tw>
2025-06-07 00:35:40 +0800

    Forgot to flip the video.

--- start-cam.sh
+++ start-cam.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-rpicam-vid --listen -o "tcp://127.0.0.1:54088" \
+rpicam-vid --listen -o "tcp://127.0.0.1:54088" -n \
     --width 256 --height 256 --framerate 15 \
+    --vflip 1 \
     --inline --codec h264 -t 0 \
-    -n
